5f6d1c2c633f4f70c5dff85b681646005af66a52
James-0521 <jamessmith0521@outlook.com>
2024-05-21 21:39:45 -0400

    fix error

--- app/layout/page.js
+++ app/layout/page.js
@@ -5,7 +5,7 @@ export default function PageLayout(){
         <HelpLink title="Docs" desc="Find in-depth information about Next.js features and API." url="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"></HelpLink>
         <HelpLink title="Learn" desc="Learn about Next.js in an interactive course with&nbsp;quizzes!" url="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"></HelpLink>
         <HelpLink title="Templates" desc="Explore starter templates for Next.js." url="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"></HelpLink>
-        <HelpLink title="Deploy" desc="Instantly deploy your Next.js site to a shareable URL with Vercel." url="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"></HelpLink>
+        <HelpLink title="Deploy" desc="Istantly deploy your Next.js site to a shareable URL with Vercel." url="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"></HelpLink>
       </div>
     )
 }
